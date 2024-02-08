b7a602f2154ab8d56c5a6980cfd68813f4f548f3
Bruno Noberto <brunonoberto@Brunos-MBP.Home>
2024-02-08 17:16:57 +0000

    Adicionado cenario negativo de prodduto e refatoracao do codigo

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,5 +1,13 @@
 /// <reference types="cypress"/>
 
+let token
+
+beforeEach(() => {
+    cy.token('fulano@qa.com', 'teste').then(tkn => {
+        token = tkn
+    })
+});
+
 describe('Funcionalidade: Produtos', () => {
     it('Listar produtos - GET', () => {
         cy.request({
@@ -12,15 +20,13 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Cadastrar produtos - POST', () => {
-        // TODO: CRIAR TOKEN DINAMICO
-        let token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZ1bGFub0BxYS5jb20iLCJwYXNzd29yZCI6InRlc3RlIiwiaWF0IjoxNzA3NDA4MDQ4LCJleHAiOjE3MDc0MDg2NDh9.yvcBFLSOmMUeRd9qSrarqIAanJRAfTypFWvLB_EbEkI"
         cy.request({
             method: 'POST',
             url: 'produtos',
             headers: {authorization: token},
             body: {
                 // TODO: CRIAR PRODUTO DINAMICO
-                "nome": "PS5",
+                "nome": "PS7",
                 "preco": 470,
                 "descricao": "Video game",
                 "quantidade": 500
