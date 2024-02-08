484c2fe92b1cabfb55403fa3f4491b09c217d829
Bruno Noberto <brunonoberto@Brunos-MBP.Home>
2024-02-08 19:48:49 +0000

    Adicionado teste de contrato

---
+++ cypress/contratos/produtos.contratos.js
@@ -0,0 +1,11 @@
+const Joi = require('joi')
+
+const produtosSchema = Joi.object({
+    produtos: Joi.array().items({
+        nome: Joi.string(),
+        preco: Joi.number(),
+        descricao: Joi.string(),
+        quantidade: Joi.number(),
+        _id: Joi.string()
+    })
+})
